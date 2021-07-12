d70eb0dd4d307f4170960e3436f4363586971815
keka3avr <kekazavr228@gmail.com>
2021-07-12 13:45:41 +1100

    initial commit

---
+++ src/__tests__/template.test.js
@@ -0,0 +1,17 @@
+import { exp } from "prelude-ls";
+import Validator from "../js/app";
+
+test.each([
+    ['8 (927) 000-00-00', '+79270000000'],
+    ['+7 960 000 00 00', '+79600000000'],
+])
+('.add($#) rus number check', (str, expected) => {
+    const result = Validator.transformNumbers(str);
+    expect(result).toBe(expected)
+})
+
+test('china number check', () => {
+    const checkStr = '+86 000 000 0000';
+    const result = Validator.transformNumbers(checkStr);
+    expect(result).toBe('+860000000000');
+})
---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+export default class Validator {
+    static transformNumbers(userNumber) {
+        if (userNumber) {
+            const dashBracketsExp = /[ ()-]/g;
+            const transformedStr = userNumber.replace(dashBracketsExp, '');
+
+            if (transformedStr.length == 11)  {
+                return transformedStr.replace(/^8/, '+7');
+            } 
+            return transformedStr;
+        }
+    }
+}
